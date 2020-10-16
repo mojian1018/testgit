fcebc58292f852e3d4accfacd2797a4f76c98fb1
mojian <760565859@qq.com>
2020-10-16 09:16:47 +0800

    修改方法名

--- src/main/java/com/demo/demo25/controller/helloGit.java
+++ src/main/java/com/demo/demo25/controller/helloGit.java
@@ -1,9 +1,13 @@
 package com.demo.demo25.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class helloGit {
 
-
-    public String helloGit(){
+    @RequestMapping("index.html")
+    public String helloGits(){
 
         return "index";
     }
